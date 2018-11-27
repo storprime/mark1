db6bdd4acc257ae267a07754df1226a13c846f99
storprime <storprime@github.com>
2018-11-27 13:06:43 -0500

    committed

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'mark1',
+    script: './index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-35-183-186-54.ca-central-1.compute.amazonaws.com',
+      key: '~/.ssh/J-dev.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:storprime/mark1.git',
+      path: '/home/ubuntu/mark1',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
