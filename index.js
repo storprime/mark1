e8a1f8fba2e436ddffe7b4ae268ae51b24c39b6b
storprime <storprime@github.com>
2018-11-28 13:13:23 -0500

    test html serve

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const express = require('express')
 const app = express ()
-app.get('/', (req,res) => {res.send('Hey, again, Justin!')})
+app.use(express.static('public'))
 app.listen (3000, () =>  console.log('Server running on port 3000'))
