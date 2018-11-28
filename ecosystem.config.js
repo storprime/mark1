f6a6142b2b4e862152c36035b55d9fda05174574
storprime <storprime@github.com>
2018-11-28 12:02:55 -0500

    Setup PM2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,7 +10,7 @@ module.exports = {
       key: '~/.ssh/J-dev.pem',
       ref: 'origin/master',
       repo: 'git@github.com:storprime/mark1.git',
-      path: '/home/ubuntu/mark1',
+      path: '/home/ubuntu/source/mark1',
       'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
     }
   }
