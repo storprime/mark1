08f20c98b19003fb085110f8598743f257478488
storprime <storprime@github.com>
2018-12-11 17:13:26 -0500

    ok, messy, but making another small change

--- Japp.js
+++ Japp.js
@@ -3,15 +3,15 @@ const app = express();
 var parse = require('url-parse');
 var pg = require('pg');
 var format = require('pg-format');
-var url = parse('postgres://admin_urbanstrategies:Oriole180*@urbanstrategies.ck0stywphz0a.ca-central-1.rds.amazonaws.com: 5432/projects_urbanstrategies',true); //this outputs a configured url
+var url = parse('postgres://admin_urbanstrategies:Oriole180*@urbanstrategies.ck0stywphz0a.ca-central-1.rds.amazonaws.com: 5432/projects_urbanstrategies',true); //this outputs a configured url. I need to hide the password. How?
 
 console.log(url)
-// const config = {
-//   username: url[1],
-//   password: url[2],
-//   hostname: url[3],
-//   port: url[4],
-//   database: a
+ // const config = {
+ //   username: url[1],
+ //   password: url[2],
+ //   hostname: url[3],
+ //   port: url[4],
+ //   database: a
 //
 //   max:  10,
 //   idleTimeoutMS: 30000,
