54b871f03e366b4255be55cb5aaf4136f02de9ac
storprime <storprime@github.com>
2018-12-11 10:23:38 -0500

    changed js and html to test PostGresql

--- Japp.js
+++ Japp.js
@@ -1,25 +1,19 @@
-var pg  = require("pg");
-var express = require ("express") //why do I have to connect to this server?
-var app = express ();
+const {Pool, Client} = require('pg');
+const connectionString = 'postgresql://admin_urbanstrategies:Oriole197*@urbanstrategies.ck0stywphz0a.ca-central-1.rds.amazonaws.com:5432/projects_urbanstrategies'
+const pool = new Pool ({
+  connectionString: connectionString,})
 
-var connectionString = "postgres://admin_urbanstrategies:Oriole197*@urbanstrategies.ck0stywphz0a.ca-central-1.rds.amazonaws.com:5432/projects_urbanstrategies"
-app.get('/', function (req,res,next) {
-  pg.connect(connectionString, function(err, client, done) {
-    if (err) {
-      console.log (err);
-      res.status(400).send (err);
-    }
-    client.query('SELECT * FROM residence', [1], function (err, result) {
-      done();
-      if (err) {
-        console.log (err);
-        res.status(400).send(err);
-      }
-      res.status(200).send(result.rows);
-    });
-  });
-});
+pool.query('SELECT NOW()',(err, res) => {
+  console.log(err, res)
+  pool.end()
+})
 
-app.listen (3000, function () {
-  console.log('Server is running on Port 3000')
-});
+const client = new Client ({connectionString: connectionString
+})
+client.connect()
+
+client.query('SELECT NOW()', (err, res) => {
+  console.log(err, res)
+  document.getElementById('output').innerHTML = "check the console"
+  client.end()
+})
